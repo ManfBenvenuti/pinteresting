888257b853a1695a098f6a910eb96f42eca817ff
Manfredi Benvenuti <manfbenvenuti@gmail.com>
2014-12-02 23:09:57 +0100

    Prova debugging Heroku-Amazon 2

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -7,7 +7,13 @@ class Pin < ActiveRecord::Base
 					:thumb => "100x100>" },             
 					
 					:storage => :s3,
-          :bucket  => ENV['manfpinteresting']
+          :bucket  => ENV['MY_BUCKET_NAME'],
+          :s3_credentials => {
+              :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+              :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                },
+          :s3_permissions => 'private',
+          :url => ":s3_domain_url"
   
   # Necessario per paperclip 4.0 in poi
   validates_attachment_content_type :image, :content_type => /\Aimage/
