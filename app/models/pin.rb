2539cd3a1b5126191dacbdf569c8ded39ed169f5
Manfredi Benvenuti <manfbenvenuti@gmail.com>
2014-12-02 23:04:52 +0100

    Debugging Heroku-Amazon S3

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,13 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :large => '1000x1000>', :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { 
+					:large => '1000x1000>', 
+					:medium => "300x300>", 
+					:thumb => "100x100>" },             
+					
+					:storage => :s3,
+          :bucket  => ENV['manfpinteresting']
   
   # Necessario per paperclip 4.0 in poi
   validates_attachment_content_type :image, :content_type => /\Aimage/
